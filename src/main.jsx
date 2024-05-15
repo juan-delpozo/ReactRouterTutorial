3ba4b385724dbe94bea6d66412f46c574e147a0e
Juan Del Pozo <83425295+juan-delpozo@users.noreply.github.com>
2024-05-15 13:25:39 -0400

    Implement data loading for contacts in root route

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
