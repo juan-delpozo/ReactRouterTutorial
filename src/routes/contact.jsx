2c2b0d473efdf103d65ff3ad9c5f0bbf7ecaea6d
Juan Del Pozo <83425295+juan-delpozo@users.noreply.github.com>
2024-05-15 15:32:41 -0400

    Add loader to contact page for fetching contact data using URL Params

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,46 +1,46 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
+
+export async function loader({ params }) {
+    const contact = await getContact(params.contactId);
+    return { contact };
+}
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+    
+    const { contact } = useLoaderData();
 
-  return (
-    <div id="contact">
-      <div>
-        <img
-          key={contact.avatar}
-          src={contact.avatar || null}
-        />
-      </div>
+    return (
+        <div id="contact">
+            <div>
+                <img
+                    key={contact.avatar}
+                    src={contact.avatar || null}
+                />
+            </div>
 
-      <div>
-        <h1>
-          {contact.first || contact.last ? (
+        <div>
+            <h1>
+                {contact.first || contact.last ? (
             <>
-              {contact.first} {contact.last}
+                {contact.first} {contact.last}
             </>
-          ) : (
-            <i>No Name</i>
-          )}{" "}
-          <Favorite contact={contact} />
-        </h1>
+            ) : (
+                <i>No Name</i>
+            )}{" "}
+                <Favorite contact={contact} />
+            </h1>
 
-        {contact.twitter && (
-          <p>
-            <a
-              target="_blank"
-              href={`https://twitter.com/${contact.twitter}`}
-            >
-              {contact.twitter}
-            </a>
-          </p>
-        )}
+            {contact.twitter && (
+            <p>
+                <a
+                target="_blank"
+                href={`https://twitter.com/${contact.twitter}`}
+                >
+                    {contact.twitter}
+                </a>
+            </p>
+            )}
 
         {contact.notes && <p>{contact.notes}</p>}
 
