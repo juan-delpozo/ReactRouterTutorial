7658f62fbaa1d34a203e17777634ca7e68a1c11f
Juan Del Pozo <83425295+juan-delpozo@users.noreply.github.com>
2024-05-18 18:29:49 -0400

    Add 404 response handling for non-existent contacts in loader

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -6,8 +6,14 @@ import {
 import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
-    const contact = await getContact(params.contactId);
-    return { contact };
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  return { contact };
 }
 
 export async function action({ request, params }) {
@@ -81,7 +87,7 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   const fetcher = useFetcher();
-  
+
   let favorite = contact.favorite;
   if (fetcher.formData) {
     favorite = fetcher.formData.get("favorite") === "true";
