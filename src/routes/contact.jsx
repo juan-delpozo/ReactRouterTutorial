1c0c6fb7832600567d86079795d9919856e146a0
Juan Del Pozo <83425295+juan-delpozo@users.noreply.github.com>
2024-05-18 18:24:19 -0400

    Implement mutation without navigation using useFetcher

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,13 +1,23 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import {
+  useLoaderData,
+  Form,
+  useFetcher,
+} from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
     const contact = await getContact(params.contactId);
     return { contact };
 }
 
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
+
 export default function Contact() {
-    
     const { contact } = useLoaderData();
 
     return (
@@ -70,10 +80,11 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-  // yes, this is a `let` for later
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -85,6 +96,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+      </fetcher.Form>
   );
 }
